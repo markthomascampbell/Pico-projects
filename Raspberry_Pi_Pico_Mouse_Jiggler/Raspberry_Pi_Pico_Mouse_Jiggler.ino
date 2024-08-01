4a4718311b262f17cb8c24e75de18d15d5a10add
Mark Campbell <35576900+markthomascampbell@users.noreply.github.com>
2024-08-01 10:02:07 -0500

    Update Raspberry_Pi_Pico_Mouse_Jiggler.ino
    Added comments, and made script more configurable.

--- Raspberry_Pi_Pico_Mouse_Jiggler/Raspberry_Pi_Pico_Mouse_Jiggler.ino
+++ Raspberry_Pi_Pico_Mouse_Jiggler/Raspberry_Pi_Pico_Mouse_Jiggler.ino
@@ -1,25 +1,31 @@
-// 
 #include <Mouse.h>
-int d = 20;
-int s = 1; 
-int t = 2;
+//#define debugEnabled                   // Uncomment to enable debug over serial
+int w = 20;                              // How many pixels' width do you want the cursor to move?
+int p = 1;                               // How many pixels at a time do you want the cursor to move?
+int d = 7;                               // How fast do you want the cursor to move? lower = faster
+int t = 2;                               // How many times do you want the cursor to move back and forth?
+int dTime = 30000;                       // The delay in between moving the cursor in milliseconds
 
 void setup() {
-  //Serial.begin(9600);
+  #if defined (debugEnabled)
+    Serial.begin(9600);
+  #endif
   Mouse.begin();
 }
 
 void loop() {
-  //Serial.println("Jiggle, Jiggle!");
-  for ( int i = 1; i <= t; i++) {
-    for (int x = 0; x <= d; x++) {
-      Mouse.move(-s, 0);
-      delay(7);
+  #if defined (debugEnabled)
+    Serial.println("Jiggle, Jiggle!");
+  #endif
+  for ( int i = 1; i <= t; i++) {        // Number of times to repeat the whole process defined by t
+    for (int x = 0; x <= w; x++) {       // Repeat the process of moving 1 pixel at a time
+      Mouse.move(-s, 0);                 // Move left
+      delay(d);                          // Delay determines apparent speed of cursor
     }
-    for (int x = 0; x <= d; x++) {
-      Mouse.move(s, 0);
-      delay(7);
+    for (int x = 0; x <= w; x++) {       // Repeat the process of moving 1 pixel at a time
+      Mouse.move(s, 0);                  // Move right
+      delay(d);                          // Delay determines apparent speed of cursor
     }
   }
-  delay(30000);
+  delay(dTime);                          // Delay determines how long to wait until next cursor jiggle
 }
