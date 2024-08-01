cd2f28d5bafc90a920cf8a3315b4dd8a35fbb41b
Mark Campbell <35576900+markthomascampbell@users.noreply.github.com>
2024-08-01 10:27:15 -0500

    Update Raspberry_Pi_Pico_Mouse_Jiggler.ino

--- Raspberry_Pi_Pico_Mouse_Jiggler/Raspberry_Pi_Pico_Mouse_Jiggler.ino
+++ Raspberry_Pi_Pico_Mouse_Jiggler/Raspberry_Pi_Pico_Mouse_Jiggler.ino
@@ -1,31 +1,31 @@
-#include <Mouse.h>
-//#define debugEnabled                   // Uncomment to enable debug over serial
-int w = 20;                              // How many pixels' width do you want the cursor to move?
-int p = 1;                               // How many pixels at a time do you want the cursor to move?
-int d = 7;                               // How fast do you want the cursor to move? lower = faster
-int t = 2;                               // How many times do you want the cursor to move back and forth?
-int dTime = 30000;                       // The delay in between moving the cursor in milliseconds
+#include <Mouse.h>                        // Load mouse/HID library
+//#define debugEnabled                    // Uncomment to enable debug over serial
+int w = 20;                               // How many pixels' width do you want the cursor to move?
+int p = 1;                                // How many pixels at a time do you want the cursor to move? a speed multiplier
+int d = 7;                                // How fast do you want the cursor to move in milliseconds? lower = faster
+int t = 2;                                // How many times do you want the cursor to move back and forth?
+int f = 30000;                            // The frequency in moving the cursor in milliseconds
 
 void setup() {
   #if defined (debugEnabled)
-    Serial.begin(9600);
+    Serial.begin(9600);                   // Enable Serial if debug is enabled
   #endif
-  Mouse.begin();
+  Mouse.begin();                          // Enable Mouse functionality
 }
 
 void loop() {
   #if defined (debugEnabled)
-    Serial.println("Jiggle, Jiggle!");
+    Serial.println("Jiggle, Jiggle!");    // If debug enabled, print serial message during every jiggle
   #endif
-  for ( int i = 1; i <= t; i++) {        // Number of times to repeat the whole process defined by t
-    for (int x = 0; x <= w; x++) {       // Repeat the process of moving 1 pixel at a time
-      Mouse.move(-s, 0);                 // Move left
-      delay(d);                          // Delay determines apparent speed of cursor
+  for ( int i = 1; i <= t; i++) {         // Number of times to repeat the whole process defined by t
+    for (int x = 0; x <= w; x+p) {        // Repeat the process of moving "p" pixel(s) at a time until "w" pixels reached
+      Mouse.move(-p, 0);                  // Move left "p" pixel(s)
+      delay(d);                           // How long to wait until we move to the next pixel? Delay determines apparent speed of cursor
     }
-    for (int x = 0; x <= w; x++) {       // Repeat the process of moving 1 pixel at a time
-      Mouse.move(s, 0);                  // Move right
-      delay(d);                          // Delay determines apparent speed of cursor
+    for (int x = 0; x <= w; x+p) {        // Repeat the process of moving "p" pixel(s) at a time until "w" pixels reached
+      Mouse.move(p, 0);                   // Move right "p" pixel(s)
+      delay(d);                           // How long to wait until we move to the next pixel? Delay determines apparent speed of cursor
     }
   }
-  delay(dTime);                          // Delay determines how long to wait until next cursor jiggle
+  delay(f);                               // Delay determines how long to wait until next cursor jiggle
 }
